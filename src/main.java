f1cc172154a4c0ca7860eee1be2d2445f1880ddb
chubbyhubbybubby <austinc2026@cis.edu.hk>
2023-09-14 17:11:38 +0800

    nice lab 1.1

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class main {
+    public static void main (String [] args){
+        int minimum_age = 18;
+        minimum_age = 7 + (minimum_age / 2);
+        System.out.println(minimum_age);
+        int age = 16;
+        int pageToRead=100-age;
+        System.out.println(pageToRead);
+    }
+
+}
