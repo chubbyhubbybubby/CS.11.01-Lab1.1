62cb1139113d76494c2262242aefecbc359cd5de
chubbyhubbybubby <austinc2026@cis.edu.hk>
2023-09-15 07:44:02 +0800

    nice lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,8 @@
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        int minimum_age = 18;
+        minimum_age = 7 + (minimum_age / 2);
+        System.out.println(minimum_age);
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,8 @@
 public class Pages {
 
     public static void main(String[] args) {
-
+        int age = 16;
+        int pageToRead=100-age;
+        System.out.println(pageToRead);
     }
 }
